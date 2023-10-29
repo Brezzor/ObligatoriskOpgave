5a2dd911508513bbb8e4d3abce54c172761eb7ed
Oliver Bresson <oliver.bresson@gmail.com>
2023-10-29 20:11:17 +0100

    basic setup for login, register and main.

--- app/src/main/java/com/example/obligatoriskopgave/RegisterActivity.kt
+++ app/src/main/java/com/example/obligatoriskopgave/RegisterActivity.kt
@@ -1,48 +1,37 @@
 package com.example.obligatoriskopgave
 
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.obligatoriskopgave.databinding.ActivityLoginBinding
+import com.example.obligatoriskopgave.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var activityIntent: Intent
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
         auth = Firebase.auth
 
-        binding.loginButton.setOnClickListener {
-            activityIntent = Intent(this, MainActivity::class.java)
-            startActivity(activityIntent)
-            finish()
-        }
-
         binding.registerButton.setOnClickListener {
-            activityIntent = Intent(this, RegisterActivity::class.java)
+            activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
             finish()
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        if (currentUser != null)
-        {
-            activityIntent = Intent(this, MainActivity::class.java)
+        binding.loginButton.setOnClickListener {
+            activityIntent = Intent(this, LoginActivity::class.java)
             startActivity(activityIntent)
             finish()
         }
